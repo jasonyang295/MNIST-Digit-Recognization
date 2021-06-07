4c446982d1419a42ce7a80e4558374f101c937c4
Jason Yang <canyoncrestjy@gmail.com>
2021-06-06 17:04:58 -0700

    big update

--- recognition.py
+++ recognition.py
@@ -11,3 +11,19 @@ x_train = tf.keras.utils.normalize(x_train, axis = 1)
 x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
 #dont do it for y, only for x
+
+
+#built model with 2 hidden layers and 1 dense output layer with softmax function
+
+model = tf.keras.models.Sequential() #initalize the grid
+model.add(tf.keras.layers.Flatten(input_shape = (28, 28)))#flatten it out and input shape is 28 x 28
+#now connect all neurons to previous and following layers with 2 hidden layers and relu activation function
+model.add(tf.keras.layers.Dense(units = 128, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(units = 128, activation = tf.nn.relu))
+
+#next line scales nums down with softmax function so they add up to 1 so we have actual percentages
+model.add(tf.keras.layers.Dense(units = 10, activation = tf.nn.softmax))
+
+
+
+
