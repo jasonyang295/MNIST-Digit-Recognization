9f5e53cffea8cc9d8d3ec40ce7c58ad08f881e6c
Jason Yang <canyoncrestjy@gmail.com>
2021-06-06 17:00:36 -0700

    updated

--- recognition.py
+++ recognition.py
@@ -4,3 +4,10 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#normalize here to scale it down
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+#dont do it for y, only for x
